c55538376e1760abc0d1bc6673f03a62badaaa8f
PaprikaSol <zoipapajim@gmail.com>
2021-02-15 05:04:38 +0200

    v.2.1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.myapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,19 +22,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        Button btn_sgn = findViewById(R.id.sign_in);
-        btn_sgn.setOnClickListener(new View.OnClickListener() {
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        Button btnSgn = findViewById(R.id.sign_in);
+        Button btnReg = findViewById(R.id.register);
+        TextInputEditText inputUser = findViewById(R.id.username_edit);
+        TextInputEditText inputPass= findViewById(R.id.password_edit);
+
+//        String txtUser = Objects.requireNonNull(inputUser.getText()).toString();
+//        String txtPass = Objects.requireNonNull(inputPass.getText()).toString();
+
+
+        btnSgn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Snackbar.make(v, "You are signing in...", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if (inputUser.getText().toString().matches("") | inputPass.getText().toString().matches("")) {
+                    Snackbar.make(v, "The fields are empty, bitch!", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                } else {
+                    Intent intent = new Intent(MainActivity.this, SuccessActivity.class);
+                    startActivity(intent);
+                }
             }
         });
 
-        Button btn_reg = findViewById(R.id.register);
-        btn_reg.setOnClickListener(new View.OnClickListener() {
+        btnReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Snackbar.make(v, "You are registering...", Snackbar.LENGTH_LONG)
